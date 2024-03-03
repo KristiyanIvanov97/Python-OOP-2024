8798d07f34c39f13781200a6e935754039eb6801
Kristiyan Ivanov <kris.ivanov97@gmail.com>
2024-03-03 22:25:41 +0200

    Added exercises

---
+++ 06_Inheritance_Exercise/05_shop/project/product_repository.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from project.drink import Drink
+from project.food import Food
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products: List[Food or Drink] = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name: str):
+        product = self.find(product_name)
+
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return "\n".join(f'{p.name}: {p.quantity}' for p in self.products)
